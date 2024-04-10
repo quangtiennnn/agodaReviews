2cdd27ad89ce0c14251b191c58d9fa9411f38588
Quang Tien Nguyen <105355188+quangtiennnn@users.noreply.github.com>
2024-04-10 19:32:23 +0700

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,17 @@
+from hotelreviewFunction import *
+
+data = pd.read_csv('city1.csv')
+
+count = 0
+new_df = split_list(data.hotel_id,4)[1] 
+numofhotel  = len(new_df.hotel_id)
+
+for hotel_id in new_df.hotel_id:
+    start = time.time()
+    hotelReviews(hotel_id)
+    end = time.time()
+    new_data = f"Elapsed time: {round(end-start, 2)} seconds <--> {count}/{numofhotel}"
+    count += 1
+    with open("p1.txt", "a") as file:
+        file.write(new_data)
+        file.write("\n")
